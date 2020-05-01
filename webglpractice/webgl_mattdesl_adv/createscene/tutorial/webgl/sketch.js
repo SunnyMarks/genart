e90d4d7e5b5d0073bea39ec672bc574de8d54214
MarkiMarkss <markimarksstudio@gmail.com>
2020-04-30 21:05:44 -0500

    attempted to add a new torus Mesh, won't show in scene

--- webglpractice/webgl_mattdesl_adv/createscene/tutorial/webgl/sketch.js
+++ webglpractice/webgl_mattdesl_adv/createscene/tutorial/webgl/sketch.js
@@ -37,10 +37,23 @@ const sketch = ({ context }) => {
   const geometry = new THREE.SphereGeometry(1, 32, 16);
 
 
+  const torus = new THREE.TorusGeometry(1, 0.5, 32, 64); //outer radius size, inner radius(donut thickness), segments
+
+
   //Create a Texture and load images
   const loader = new THREE.TextureLoader();
   const texture = loader.load("earth.jpg");
   const moonTexture = loader.load("moon.jpg");
+
+  //Create a Texture for a torus geometry;
+  //load in diffuse (color) map(image)
+  const torusTexture = loader.load("brick-diffuse.jpg");
+  //fix stretching
+  //along the horizontal and vertical make sure it wraps seemlessly *
+  torusTexture.wrapS = torusTexture.wrapT = THREE.RepeatWrapping;
+  //repeat the texture to reduce stretching further along the two axis.
+  torusTexture.repeat.set(2, 1).multiplyScalar(2); //scales the repeat vector(2,1)
+
 
   // Setup a material
   const material = new THREE.MeshStandardMaterial({
@@ -65,8 +78,21 @@ const sketch = ({ context }) => {
   moonMesh.position.set(1.5, 1, 0);
   moonMesh.scale.setScalar(0.25);
 
+  //Create a torus material & mesh, and add it to moonGroup
+  const torusMaterial = new THREE.MeshStandardMaterial({
+    roughness: 1,
+    metalness: 0,
+    map: torusTexture
+  });
+
+  const torusMesh = new THREE.Mesh(torus, torusMaterial);
+  torusMesh.position.set(2.5, 1, 0);
+  torusMesh.scale.set(0.50);
+
+
   //Add mesh to group
   moonGroup.add(moonMesh);
+  moonGroup.add(torusMesh);
 
   //Add group to scene
   scene.add(moonGroup);
